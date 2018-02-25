763f56e1eb03cf3b821982c5734e19752513e949
Burak Yuksel <beyuksel96@gmail.com>
2018-02-25 20:31:07 +0000

    Changes made to login.css and login.js login.css: added font-size to the active <a> tag login.js: made transitions more smooth.

--- js/login.js
+++ js/login.js
@@ -1,15 +1,15 @@
 $(function() {
 
     $('#student-form').click(function(e) {
-		$("#student-login-form").delay(100).fadeIn(100);
- 		$("#demon-login-form").fadeOut(100);
+		$("#student-login-form").delay(250).fadeIn(250);
+ 		$("#demon-login-form").fadeOut(250);
 		$('#demon-form').removeClass('active');
 		$(this).addClass('active');
 		e.preventDefault();
 	});
 	$('#demon-form').click(function(e) {
-		$("#demon-login-form").delay(100).fadeIn(100);
- 		$("#student-login-form").fadeOut(100);
+		$("#demon-login-form").delay(250).fadeIn(250);
+ 		$("#student-login-form").fadeOut(250);
 		$('#student-form').removeClass('active');
 		$(this).addClass('active');
 		e.preventDefault();
