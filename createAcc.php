47f1195bcdd04445fa206e504b9485cc84014d40
Burak Yuksel <beyuksel96@gmail.com>
2018-02-28 19:45:05 +0000

    Changed contents of this file so that it now:  - Checks if a username/account name already exists. If it does then a message is returned to the client saying just that and the account is not created  - As mentioned in the last point. Messages are now returned to the file which posted the data to this php file. These messages are helpful success or error messages which are then displayed to the client.  - Few other minor changes

--- createAcc.php
+++ createAcc.php
@@ -1,18 +1,33 @@
 <?php
     include('connection.php');
 
-    $salt = uniqid(mt_rand(), true);
-    $salted = $salt . $_POST['psw'];
-    $hashed = hash('sha512', $salted);
+    if(isset($_POST['accName'])){
+        $accName = mysqli_real_escape_string($connection, $_POST['accName']); 
+        $accountType = mysqli_real_escape_string($connection, $_POST['accType']);
+        $password = mysqli_real_escape_string($connection, $_POST['psw']);
 
-    $accName=$_POST['accName']; 
-    $accountType=$_POST['accType'];
+        $salt = uniqid(mt_rand(), true);
+        $salted = $salt . $password;
+        $hashed = hash('sha512', $salted);
 
-    $sql = mysqli_query($connection, "INSERT INTO users(Username, PasswordSalt, PasswordHash, account_type) VALUES ('$accName','$salt','$hashed','$accountType')");
-    if(!$sql){
-        echo "Error" . mysqli_error();
+        //check if account name exists in database, if yes then prompt user
+        $accCheck = mysqli_query($connection, "SELECT * FROM users WHERE Username = '$accName'"); //checking if username already exists in db
+        if(mysqli_num_rows($accCheck) != 0) {
+            $response = json_encode(array('type' => 'error', 'text' => 'Username already exists: Try using a different username...')); //message to send back to client side
+            die($response);
+        }
+        else {
+            $sql = mysqli_query($connection, "INSERT INTO users(Username, PasswordSalt, PasswordHash, account_type) VALUES ('$accName','$salt','$hashed','$accountType')");
+            if(!$sql) {
+                $output = "Error" . mysqli_error();
+                $response = json_encode(array('type' => 'error', 'text' => $output));
+                die($response);
+            }
+            else {
+                $response = json_encode(array('type' => 'success', 'text' => 'Success: Account has been created!'));
+                die($response);
+            }
+        }
     }
-    else{
-        echo "account created";
-    }
+    mysqli_close($connection);
 ?>
