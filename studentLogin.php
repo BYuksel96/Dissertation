230ccd71d2e444ba6f681bef0192a76fe870deda
Burak Yuksel <beyuksel96@gmail.com>
2018-03-02 18:35:49 +0000

    This file now contains code which - working:  - Allows students to login to the web app  - Specific account data is stored in the db  - Returns an error if an account already exists  - Also reports on any errors, if there are any, when inserting data

--- studentLogin.php
+++ studentLogin.php
@@ -1,3 +1,28 @@
 <?php
 
+include('connection.php');
+
+if (isset($_POST['name'])){
+    $studentName = mysqli_real_escape_string($connection, $_POST['name']);
+    $studentID = mysqli_real_escape_string($connection, $_POST['stuNum']);
+
+    $accCheck = mysqli_query($connection, "SELECT * FROM students WHERE studentid = '$studentID'");
+    if (mysqli_num_rows($accCheck) != 0){
+        $response = json_encode(array('type' => 'error', 'text' => 'That student number is already in use...')); //message to send back to client side
+        die($response);
+    }
+    else {
+        $sql = mysqli_query($connection, "INSERT INTO students(studentid, studentname) VALUES ('$studentID','$studentName')");
+        if(!$sql) {
+            $output = "Error creating account. Error message: " . mysqli_error();
+            $response = json_encode(array('type' => 'error', 'text' => $output));
+            die($response);
+        }
+        else {
+            $response = json_encode(array('type' => 'success', 'text' => 'Success: Account has been created!'));
+            die($response);
+        }
+    }
+}
+mysqli_close($connection);
 ?>
