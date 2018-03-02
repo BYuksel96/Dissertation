47fd820198efcc29b81eae0ee0ff20e24f832d0a
Burak Yuksel <beyuksel96@gmail.com>
2018-03-02 18:33:39 +0000

    Added function to allow students to now login to the web app

--- js/login.js
+++ js/login.js
@@ -19,6 +19,25 @@ $(function() { //initialise on load
             }
         });
     });
+    $('#student-login-form').on('submit', function(e) { //function is called when login button is pressed
+        
+        e.preventDefault(); //prevents page from opening - so basically stops the page it is currently on from refreshing
+
+        $.ajax({
+            type: 'POST', //get or post? this time we want to post data to the php file
+            url: 'studentLogin.php', //php we send the data login data to
+            dataType: 'json',
+            data: $('#student-login-form').serialize(), //takes contents of the form
+            success : function (data) { 
+                if(data.type == 'error'){ //if username does not exist or password is inccorect an error message is displayed
+                    document.getElementById("msg-response-stu").innerHTML=data.text;
+                }
+                else { //if all is successful demonstrator is redirected to the main page
+                    window.location.replace("main.php");
+                }
+            }
+        });
+    });
     $('#student-form').click(function(e) {
 		$("#student-login-form").delay(250).fadeIn(250);
  		$("#demon-login-form").fadeOut(250);
