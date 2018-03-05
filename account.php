115db0260d274afd9534cc0d2c186ba9d2b2a362
Burak Yuksel <beyuksel96@gmail.com>
2018-03-05 22:34:55 +0000

    Account page now is styled differently. It now consists of a working bootstrap accordoin style. More content has also been added to account.php - some functionality of newly added content is currently not in use. CSS for the accounts page has also been editted slightly to improve the pages looks a little bit more.

--- account.php
+++ account.php
@@ -30,46 +30,138 @@
                 <li><a class="nav-link" href="logout.php"><i class="fa fa-sign-out"></i> Logout</a></li>
             </ul>
         </nav>
-       
-        <div class="accountInfo">
-            
-        </div>
 
-        <div class="changePsw">
-                
-        </div>
-        <?php if ($_SESSION["accType"] == "admin") { ?>
-            <div class="createAcc">
-                <form id="crtAcc" name="crtAcc" style="border:1px solid #ccc">
-                    <div class="container-fluid">
-                        <h1>Create Account</h1>
-                        <p>Fill in the form below to create an account for a demonstrator</p>
-                        <hr class="formHR">
+        <div id="accordion">
 
-                        <div class="form-group">
-                            <label for="accName"><b>Account Name:</b></label>
-                            <input type="text" placeholder="Enter Account Name" name="accName" id="accName" class="form-control" required>
+            <div class="card">
+                <div class="card-header">
+                    <a class="collapsed card-link" data-toggle="collapse" data-parent="#accordion" href="#collapseOne">Account Info</a>
+                </div>
+                <div id="collapseOne" class="collapse">
+                    <form id="addData" class="card-body" name="addData">
+                        <div class="container-fluid">
+                            <p>The form below is where you can supply the data you expect students to fill out before they make a help request.</p>
+                            <hr class="formHR">
+
+                            <div class="form-group">
+                                <label for="weekSub"><b>Enter week submission # or enter specific help request data (i.e. Exam Question):</b></label>
+                                <input type="text" placeholder="Enter data here" name="weekSub" id="weekSub" class="form-control" required>
+                            </div>
+                            
+                            <div class="form-group">
+                                <label for="taskNum"><b>Enter the max task number or enter specific help request data (i.e. Exam Question):</b></label>
+                                <input type="password" placeholder="Enter Password" name="taskNum" id="taskNum" class="form-control" required>
+                            </div>
+
+                            <p id="msg-response2" style="color: green; font-size: 10pt;"></p>
+                            <button type="submit" id="submitButton" class="btn btn-success">Add data</button>
+
                         </div>
-                        
-                        <div class="form-group">
-                            <label for="psw"><b>Enter Account Password:</b></label>
-                            <input type="password" placeholder="Enter Password" name="psw" id="psw" class="form-control" required>
-                        </div>
-
-                        <div class="form-group">
-                            <label for="accType"><b>Choose Account Type:</b></label>
-                            <select name="accType" id="accType" class="form-control" required>
-                                <option value="admin">Admin Account</option>
-                                <option value="standard">Standard Account</option>
-                            </select>
-                        </div>
-
-                        <p id="msg-response" style="color: green; font-size: 10pt;"></p>
-                        <button type="submit" id="submitAcc" class="btn btn-success">Create Account</button> <!-- will need to sort out the css form this button as can not use 'button' in the css file as the selector for editting the button settings as it would edit it for all button tags -->
-
-                    </div>
-                </form>
+                    </form>
+                </div>
             </div>
-        <?php } ?>
+
+            <div class="gap"></div>
+
+            <div class="card">
+                <div class="card-header">
+                    <a class="collapsed card-link" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo">Change Password</a>
+                </div>
+                <div id="collapseTwo" class="collapse">
+                    <form id="addData" class="card-body" name="addData">
+                        <div class="container-fluid">
+                            <p>The form below is where you can supply the data you expect students to fill out before they make a help request.</p>
+                            <hr class="formHR">
+
+                            <div class="form-group">
+                                <label for="weekSub"><b>Enter week submission # or enter specific help request data (i.e. Exam Question):</b></label>
+                                <input type="text" placeholder="Enter data here" name="weekSub" id="weekSub" class="form-control" required>
+                            </div>
+                            
+                            <div class="form-group">
+                                <label for="taskNum"><b>Enter the max task number or enter specific help request data (i.e. Exam Question):</b></label>
+                                <input type="password" placeholder="Enter Password" name="taskNum" id="taskNum" class="form-control" required>
+                            </div>
+
+                            <p id="msg-response2" style="color: green; font-size: 10pt;"></p>
+                            <button type="submit" id="submitButton" class="btn btn-success">Add data</button>
+
+                        </div>
+                    </form>
+                </div>
+            </div>
+
+            <div class="gap"></div>
+
+            <?php if ($_SESSION["accType"] == "admin") { ?>
+
+                <div class="card">
+                    <div class="card-header">
+                        <a class="collapsed card-link" data-toggle="collapse" data-parent="#accordion" href="#collapseThree">Create Account</a>
+                    </div>
+                    <div id="collapseThree" class="collapse">
+                        <form id="crtAcc" class="class-body" name="crtAcc">
+                            <div class="container-fluid">
+                                <p>Fill in the form below to create an account for a demonstrator</p>
+                                <hr class="formHR">
+
+                                <div class="form-group">
+                                    <label for="accName"><b>Account Name:</b></label>
+                                    <input type="text" placeholder="Enter Account Name" name="accName" id="accName" class="form-control" required>
+                                </div>
+                                
+                                <div class="form-group">
+                                    <label for="psw"><b>Enter Account Password:</b></label>
+                                    <input type="password" placeholder="Enter Password" name="psw" id="psw" class="form-control" required>
+                                </div>
+
+                                <div class="form-group">
+                                    <label for="accType"><b>Choose Account Type:</b></label>
+                                    <select name="accType" id="accType" class="form-control" required>
+                                        <option value="admin">Admin Account</option>
+                                        <option value="standard">Standard Account</option>
+                                    </select>
+                                </div>
+
+                                <p id="msg-response" style="color: green; font-size: 10pt;"></p>
+                                <button type="submit" id="submitAcc" class="btn btn-success">Create Account</button> <!-- will need to sort out the css form this button as can not use 'button' in the css file as the selector for editting the button settings as it would edit it for all button tags -->
+
+                            </div>
+                        </form>
+                    </div>
+                </div>
+
+                <div class="gap"></div>
+
+                <div class="card">
+                    <div class="card-header">
+                        <a class="collapsed card-link" data-toggle="collapse" data-parent="#accordion" href="#collapseFour">Help Request Data</a>
+                    </div>
+                    <div id="collapseFour" class="collapse">
+                        <form id="addData" class="card-body" name="addData">
+                            <div class="container-fluid">
+                                <p>The form below is where you can supply the data you expect students to fill out before they make a help request.</p>
+                                <hr class="formHR">
+
+                                <div class="form-group">
+                                    <label for="weekSub"><b>Enter week submission # or enter specific help request data (i.e. Exam Question):</b></label>
+                                    <input type="text" placeholder="Enter data here" name="weekSub" id="weekSub" class="form-control" required>
+                                </div>
+                                
+                                <div class="form-group">
+                                    <label for="taskNum"><b>Enter the max task number or enter specific help request data (i.e. Exam Question):</b></label>
+                                    <input type="password" placeholder="Enter Password" name="taskNum" id="taskNum" class="form-control" required>
+                                </div>
+
+                                <p id="msg-response2" style="color: green; font-size: 10pt;"></p>
+                                <button type="submit" id="submitButton" class="btn btn-success">Add data</button>
+
+                            </div>
+                        </form>
+                    </div>
+                </div>
+
+            <?php } ?>
+        </div>
     </body>
 </html>
