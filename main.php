0095841ccf9d712d9bfa025cfb5025098bdf2562
Burak Yuksel <beyuksel96@gmail.com>
2018-03-07 15:51:07 +0000

    Changed the layout, added verification for being able to access the account page, also created the first set of seating arrangements for the queue management system.

--- main.php
+++ main.php
@@ -23,17 +23,42 @@
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-        
+        <link rel="stylesheet" type="text/css" href="css/main.css">
+
     </head>
 
     <body class="container-fluid">
-    <nav class="navbar navbar-expand-sm bg-light navbar-light">
-        <ul class="nav navbar-nav ml-auto">
-            <li><a class="nav-link" href="logout.php"><i class="fa fa-sign-out"></i> Logout</a></li>
-        </ul>
-    </nav>
-    <h1>hello</h1>
-    <a href="account.php">Account</a>
-    <p><?php echo $_SESSION['accType'] ?></p>
+        <nav class="navbar navbar-expand-sm bg-light navbar-light">
+            <ul class="nav navbar-nav ml-auto">
+                <?php if (($_SESSION["accType"] == "admin") || ($_SESSION["accType"] == "standard")) { ?><li><a class="nav-link" href="account.php"><i class="fa fa-user-circle"></i> Account</a></li><?php } ?>
+                <li><a class="nav-link" href="logout.php"><i class="fa fa-sign-out"></i> Logout</a></li>
+            </ul>
+        </nav>
+
+        <div class="center">
+            <div class="table">
+                <div class="seats">
+                    A
+                </div>
+                <div class="seats">
+                    <button type="submit" id="seat1" class="btn btn-outline-success">1</button>
+                </div>
+                <div class="seats">
+                    <button type="submit" id="seat2" class="btn btn-outline-success">2</button>
+                </div>
+                <div class="seats">
+                    <button type="submit" id="seat3" class="btn btn-outline-success">3</button>
+                </div>
+                <div class="seats">
+                    <button type="submit" id="seat4" class="btn btn-outline-success">4</button>
+                </div>
+                <div class="seats">
+                    <button type="submit" id="seat5" class="btn btn-outline-success">5</button>
+                </div>
+                <div class="seats">
+                    <button type="submit" id="seat6" class="btn btn-outline-success">6</button>
+                </div>
+            </div>
+        </div>
     </body>
 </html>
