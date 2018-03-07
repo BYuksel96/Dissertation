ccea7680adf664500815fb142b63b511ac8ceb77
Burak Yuksel <beyuksel96@gmail.com>
2018-03-07 19:14:05 +0000

    Created a form which is displayed in a bootstrap modal (not full complete, needs a bit of editting to be done on it). Created some javascript to also pass seat information into the form.

--- main.php
+++ main.php
@@ -24,6 +24,12 @@
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
         <link rel="stylesheet" type="text/css" href="css/main.css">
+        <script>
+            function addSeat(objButton) {
+                var x = objButton.value;
+                $("#seatFill").val(x);
+            }
+        </script>
 
     </head>
 
@@ -41,22 +47,85 @@
                     A
                 </div>
                 <div class="seats">
-                    <button type="submit" id="seat1" class="btn btn-outline-success">1</button>
+                    <button type="submit" id="seat1" class="btn btn-outline-success" data-toggle="modal" data-target="#myModal" value="A1" onclick="addSeat(this)">1</button>
                 </div>
                 <div class="seats">
-                    <button type="submit" id="seat2" class="btn btn-outline-success">2</button>
+                    <button type="submit" id="seat2" class="btn btn-outline-success" data-toggle="modal" data-target="#myModal" value="A2" onclick="addSeat(this)">2</button>
                 </div>
                 <div class="seats">
-                    <button type="submit" id="seat3" class="btn btn-outline-success">3</button>
+                    <button type="submit" id="seat3" class="btn btn-outline-success" data-toggle="modal" data-target="#myModal" value="A3" onclick="addSeat(this)">3</button>
                 </div>
                 <div class="seats">
-                    <button type="submit" id="seat4" class="btn btn-outline-success">4</button>
+                    <button type="submit" id="seat4" class="btn btn-outline-success" data-toggle="modal" data-target="#myModal" value="A4" onclick="addSeat(this)">4</button>
                 </div>
                 <div class="seats">
-                    <button type="submit" id="seat5" class="btn btn-outline-success">5</button>
+                    <button type="submit" id="seat5" class="btn btn-outline-success" data-toggle="modal" data-target="#myModal" value="A5" onclick="addSeat(this)">5</button>
                 </div>
                 <div class="seats">
-                    <button type="submit" id="seat6" class="btn btn-outline-success">6</button>
+                    <button type="submit" id="seat6" class="btn btn-outline-success" data-toggle="modal" data-target="#myModal" value="A6" onclick="addSeat(this)">6</button>
+                </div>
+            </div>
+        </div>
+
+        <!-- Bootstrap Modal - used for students to fill out form containing help request data -->
+
+        <div class="modal fade" id="myModal">
+            <div class="modal-dialog modal-dialog-centered">
+                <div class="modal-content">
+                
+                    <!-- Modal Header -->
+                    <div class="modal-header">
+                        <h4 class="modal-title">Help Request Form</h4>
+                        <button type="button" class="close" data-dismiss="modal">&times;</button>
+                    </div>
+                    
+                    <form id="help" class="class-body" name="help">
+                        <!-- Modal body -->
+                        <div class="modal-body">
+                            <div class="form-group">
+                                <label for="weekSub"><b>Choose the category you need help with:<b style="color: red">*</b></b></label>
+                                <select name="weekSub" id="weekSub" class="form-control" required>
+                                    <option value="admin">Admin Account</option>
+                                    <option value="standard">Standard Account</option>
+                                </select>
+                            </div>
+                            <div class="form-group">
+                                <label for="taskNo"><b>Choose the sub category:<b style="color: red">*</b></b></label>
+                                <select name="taskNo" id="taskNo" class="form-control" required>
+                                    <option value="admin">Admin Account</option>
+                                    <option value="standard">Standard Account</option>
+                                </select>
+                            </div>
+                            <div class="form-group">
+                                <label for="severity"><b>Select Problem Severity:<b style="color: red">*</b></b></label>
+                                <select name="severity" id="severity" class="form-control" required>
+                                    <option value="admin">Admin Account</option>
+                                    <option value="standard">Standard Account</option>
+                                </select>
+                            </div>
+                            <div class="form-group">
+                                <label for="time"><b>Time Allocation for Task:<b style="color: red">*</b></b></label>
+                                <select name="time" id="time" class="form-control" required>
+                                    <option value="admin">Admin Account</option>
+                                    <option value="standard">Standard Account</option>
+                                </select>
+                            </div>
+                            <div class="form-group">
+                                <label for="description"><b>Brief Problem Description:</b></label>
+                                <input type="text" placeholder="Description" name="description" id="description" class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <label for="seat"><b>Seat:</b></label>
+                                <input type="text" id="seatFill" name="description" id="description" class="form-control" disabled>
+                            </div>
+                        </div>
+                        
+                        <!-- Modal footer -->
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        </div>
+                    </form>
+
                 </div>
             </div>
         </div>
