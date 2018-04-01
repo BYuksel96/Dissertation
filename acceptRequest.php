9d636dea4d25cd9b74b67333b2d199b8b64109ed
Burak Yuksel <beyuksel96@gmail.com>
2018-04-01 14:14:28 +0100

    File created to handle a demonstrators request to attend to a student.
    All working. When this file is called, certain data is stored in a db and the students request is taken off the main table and then they should be attended to.

---
+++ acceptRequest.php
@@ -0,0 +1,54 @@
+<?php
+    include('connection.php');
+    session_start();
+
+    //ticketnum
+    //stunum
+    //demonid
+    if(isset($_POST['itemNum'])){
+
+        $itemID = $_POST['itemNum'];
+        $demName = $_SESSION["demonstrator"]; //demonstrator username - used to acquire ID
+
+        // select query to acquire demon id
+        $sqlQueryID = mysqli_query($connection, "SELECT ID FROM users WHERE Username = '$demName'"); //fidning student number associated with the ticket number
+        $resultDemID = mysqli_fetch_assoc($sqlQueryID);
+        // select query to acquire student id
+        $sqlQueryStuNum = mysqli_query($connection, "SELECT StudentID FROM help_request WHERE TicketNo = '$itemID'"); //fidning student number associated with the ticket number
+        $resultStuID = mysqli_fetch_assoc($sqlQueryStuNum); //fetching the result
+        if (!$sqlQueryStuNum) {
+            $response = json_encode(array('type' => 'error', 'text' => 'Student has deleted their help request and is no longer in the queue'));
+            die($response);
+        }else{
+            $demonID = $resultDemID["ID"];
+            $stuID = $resultStuID["StudentID"];
+            if ($stuID == ""){
+                $response = json_encode(array('type' => 'error', 'text' => 'Student has deleted their help request and is no longer in the queue'));
+                die($response);
+            } else {
+                $sqlInsert = mysqli_query($connection, "INSERT INTO help_completed(users_id, student_id, ticket_no) VALUES ('$demonID','$stuID','$itemID')");
+                if(!$sqlInsert) {
+                    $output = "Error" . mysqli_error();
+                    $response = json_encode(array('type' => 'error', 'text' => $output));
+                    die($response);
+                }
+                else {
+                    //change active check to false
+                    $changeState = mysqli_query($connection, "UPDATE help_request SET active_check = \"FALSE\" WHERE TicketNo = '$itemID'");
+                    if(!$changeState) {
+                        $output = "Error" . mysqli_error();
+                        $response = json_encode(array('type' => 'error', 'text' => $output));
+                        die($response);
+                    } else {
+                        $response = json_encode(array('type' => 'success', 'text' => 'Success: Please now attend to the student.'));
+                        die($response);
+                    }
+                }
+            }
+        }
+    }else{
+        $response = json_encode(array('type' => 'error', 'text' => 'Student has deleted their help request and is no longer in the queue'));
+        die($response);
+    }
+    mysqli_close($connection);
+?>
