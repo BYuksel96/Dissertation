1d6a1282ac8273aaa52d6810993714d9251aa5de
Burak Yuksel <beyuksel96@gmail.com>
2018-04-03 20:11:05 +0100

    Added functionality so when the change password button is clicked, the js responsible for listening out for that call picks up on it, posts the data to changePassword.php and waits for a response. - All working

--- js/account.js
+++ js/account.js
@@ -47,4 +47,35 @@ $(function () { //waits for page to load before js function works
             }
         });
     });
+    //function for changing the users password password
+    $('#changePSW').on('submit', function(e) { //function is called when create account button is pressed
+
+        e.preventDefault(); //prevents page from opening
+
+        $.ajax({
+            type: 'POST', //get or post? this time we want to post data to the php file
+            url: 'changePassword.php', //php we send the data to
+            dataType: 'json',
+            data: $('#changePSW').serialize(), //takes contents of the form
+            success : function (data) { 
+                if(data.type == 'error'){ //if username exists or there is a sql error then this an error message will be displayed
+                    $("#changePSW")[0].reset();
+                    $("#msg-responsepsw").text("");
+                    $("#msg-response3").css("color", "red");
+                    document.getElementById("msg-response3").innerHTML=data.text;
+                }
+                else if(data.type =='old'){
+                    $("#changePSW")[0].reset();
+                    $("#msg-responsepsw").css("color", "red");
+                    document.getElementById("msg-responsepsw").innerHTML=data.text;
+                }
+                else{ //if account is created successfully then a message will appear saying just that
+                    $("#changePSW")[0].reset();
+                    $("#msg-responsepsw").text("");
+                    $("#msg-response3").css("color", "green");
+                    document.getElementById("msg-response3").innerHTML=data.text;
+                }
+            }
+        });
+    });
 });
