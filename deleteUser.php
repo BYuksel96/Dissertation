fa4261c45d7d7332b1e750aa0e5482b4c1b66134
Burak Yuksel <beyuksel96@gmail.com>
2018-04-04 18:52:15 +0100

    File created to allow admins to delete users (i.e. helpers) from the system as needed when remove helper button is pressed on the account.php page

---
+++ deleteUser.php
@@ -0,0 +1,19 @@
+<?php
+    include('connection.php');
+    session_start();
+
+    if (isset($_POST['user'])){
+
+        $user = $_POST['user'];
+        $query = "DELETE FROM users WHERE ID = '$user'";
+        if (mysqli_query($connection,$query)){
+            $response = json_encode(array('type' => 'success', 'text' => 'User has now been removed')); //message to send back to client side
+            die($response);
+        } else {
+            $response = json_encode(array('type' => 'error', 'text' => 'User is already deleted or this action cannot be carried out currently')); //message to send back to client side
+            die($response);
+        }
+    }
+
+    mysqli_close($connection);
+?>
