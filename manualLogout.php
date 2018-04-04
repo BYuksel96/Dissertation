a0d16e34146006bc87cf3638e1eae435c16fc850
Burak Yuksel <beyuksel96@gmail.com>
2018-04-04 20:46:01 +0100

    Fill created for an admin to manually log a students account out of the system, if needed.

---
+++ manualLogout.php
@@ -0,0 +1,21 @@
+<?php
+    include('connection.php');
+    session_start();
+
+    if (isset($_POST['id'])){
+        $studentID = $_POST['id'];
+        $result =  "DELETE FROM students WHERE StudentID = '$studentID'";
+        if(mysqli_query($connection,$result)){
+            $response = json_encode(array('type' => 'success', 'text' => 'Logged student out.')); //message to send back to client side
+            die($response);
+        }else {
+            $response = json_encode(array('type' => 'error', 'text' => 'Was not able to log student out.')); //message to send back to client side
+            die($response);
+        }
+        
+    } else {
+        $response = json_encode(array('type' => 'error', 'text' => 'Was not able to log student out.')); //message to send back to client side
+        die($response);
+    }
+
+?>
