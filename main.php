0c0d86406442918934eae3c64c8ca5f78bd21f5c
Burak Yuksel <beyuksel96@gmail.com>
2018-04-13 11:00:26 +0300

    Changed the styling of the table on the main page(main.php).

--- main.php
+++ main.php
@@ -18,7 +18,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <link rel="shortcut icon" type="image/x-icon" href="favicon.ico" />
+        <link rel="shortcut icon" type="image/x-icon" href="bufavicon.ico" />
 
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
@@ -64,7 +64,7 @@
             </div>
         </div>
 
-        <div id="studentTab" class="container table-responsive">
+        <div id="studentTab" class="container-fluid table-responsive">
             
             <?php 
             
